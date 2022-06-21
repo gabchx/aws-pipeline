c87b38ffb157cc79abc5920fb0117538d040e413
Gabriel C <gabriel.chaiix@gmail.fr>
2022-06-21 14:58:27 +0200

    Code Java Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/* Ceci est un exemple d'un programme en Java. 
+Nom de fichier : "HelloWorld.java". */
+class HelloWorld 
+{
+// Votre programme commence par main().
+// Écrire "Hello, World" dans le terminal de window.
+    public static void main(String args[]) { 
+    System.out.println("Nous allons faire un pipeline fonctionelle.");
+    }
+}
